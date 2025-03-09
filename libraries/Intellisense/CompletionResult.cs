62bdfb538af94f5db24e170a760b1e48275012e9
* <tbui17@users.noreply.github.com>
2025-03-08 17:57:16 -0700

    Refactor completion result immutable

--- libraries/Intellisense/CompletionResult.cs
+++ libraries/Intellisense/CompletionResult.cs
@@ -6,7 +6,7 @@ public record CompletionResult
 {
     public static readonly CompletionResult Empty = new();
 
-    public IEnumerable<IntellisenseEntry> Entries = ImmutableArray<IntellisenseEntry>.Empty;
-    public string Prefix = string.Empty;
-    public int Rewind;
+    public IEnumerable<IntellisenseEntry> Entries { get; init; } = ImmutableArray<IntellisenseEntry>.Empty;
+    public string Prefix { get; init; } = string.Empty;
+    public int Rewind { get; init; }
 }
