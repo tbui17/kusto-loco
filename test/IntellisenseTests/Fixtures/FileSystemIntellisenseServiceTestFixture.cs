8cccecd3080ee016f05c6850f8d9e456edf4e4b1
* <tbui17@users.noreply.github.com>
2025-04-15 21:12:18 -0600

    add unix handling for RootedPath.IsRootDirectory, fix broken tests

--- test/IntellisenseTests/Fixtures/FileSystemIntellisenseServiceTestFixture.cs
+++ test/IntellisenseTests/Fixtures/FileSystemIntellisenseServiceTestFixture.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.IO.Abstractions.TestingHelpers;
+using System.Linq;
 using Intellisense;
 using Intellisense.FileSystem;
+using Intellisense.FileSystem.Paths;
 using Microsoft.Extensions.Logging.Testing;
 
 namespace IntellisenseTests.Fixtures;
@@ -11,22 +13,33 @@ internal class FileSystemIntellisenseServiceTestFixture
     private readonly FileSystemIntellisenseService _fileSystemIntellisenseService;
     private readonly FakeLogger<IFileSystemIntellisenseService> _logger;
 
-    public FileSystemIntellisenseServiceTestFixture(Dictionary<string, MockFileData> fileData, MockFileSystemOptions? options = null)
+    public FileSystemIntellisenseServiceTestFixture(
+        Dictionary<string, MockFileData> fileData,
+        MockFileSystemOptions? options = null
+    )
     {
+        fileData = fileData.ToDictionary(
+            x => MockUnixSupport.Path(x.Key),
+            x => x.Value
+        );
         options ??= new MockFileSystemOptions { CreateDefaultTempDir = false };
         var fileSystem = new MockFileSystem(fileData, options);
         var reader = new FileSystemReader(fileSystem);
         _logger = new FakeLogger<IFileSystemIntellisenseService>();
-        _fileSystemIntellisenseService = new FileSystemIntellisenseService(reader,_logger);
+        _fileSystemIntellisenseService = new FileSystemIntellisenseService(reader, _logger, new RootedPathFactory());
     }
 
     public FileSystemIntellisenseServiceTestFixture(IFileSystemReader reader)
     {
         _logger = new FakeLogger<IFileSystemIntellisenseService>();
-        _fileSystemIntellisenseService = new FileSystemIntellisenseService(reader,_logger);
+        _fileSystemIntellisenseService = new FileSystemIntellisenseService(reader, _logger, new RootedPathFactory());
     }
 
     public IReadOnlyList<FakeLogRecord> GetLogs() => _logger.Collector.GetSnapshot();
 
-    public CompletionResult GetPathIntellisenseOptions(string path) => _fileSystemIntellisenseService.GetPathIntellisenseOptions(path);
+    public CompletionResult GetPathIntellisenseOptions(string path)
+    {
+        path = MockUnixSupport.Path(path);
+        return _fileSystemIntellisenseService.GetPathIntellisenseOptions(path);
+    }
 }
