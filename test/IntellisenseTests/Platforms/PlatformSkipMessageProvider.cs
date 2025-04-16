fe799d6e96c06bd9db5c8019e732d5c2117cda4b
* <tbui17@users.noreply.github.com>
2025-04-15 21:12:18 -0600

    add unix handling for RootedPath.IsRootDirectory, fix broken tests

---
+++ test/IntellisenseTests/Platforms/PlatformSkipMessageProvider.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.InteropServices;
+
+namespace IntellisenseTests.Platforms;
+
+public static class PlatformSkipMessageProvider
+{
+    public static string GetWindowsSkipMessage() => RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+        ? string.Empty
+        : "Windows tests are not run on non-Windows platforms";
+}
